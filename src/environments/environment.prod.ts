05edfd3d32af3f284e2a6e6ab2ec4e4ae090cc73
sonali-code-base <sonalipatwardhan2017@gmail.com>
2022-02-18 17:37:44 -0800

    Added firebase config details to env.prod.ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBiV_kgYiY4M5JsIJzjRJgFvLFMqvKDJjI",
+    authDomain: "srb-angular-todolist-project.firebaseapp.com",
+    projectId: "srb-angular-todolist-project",
+    storageBucket: "srb-angular-todolist-project.appspot.com",
+    messagingSenderId: "118913917112",
+    appId: "1:118913917112:web:f45a35c38efe2e3a8b944d",
+    measurementId: "G-TFBERLV2FT"
+  }
 };
